4072aa5c34d62656589ecf6a4959c58001d7d7ba
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-04 20:57:32 +0800

    fix:udpate

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
     <h1>CI CD 构建成功</h1>
+  <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
