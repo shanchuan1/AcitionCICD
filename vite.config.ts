381706e2f660641257c5e42dc6d4683b8ef2259c
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-04 19:59:54 +0800

    fix:update

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base:'./',// 将根路径换成相对路径
-  plugins: [vue()],
-})
