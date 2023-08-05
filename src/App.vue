b990adc3305b46cd4a20107350081ab0ec3c3f2b
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-05 12:42:28 +0800

    fix:update

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,15 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
     <h1>CI CD 构建成功</h1>
     <h2>GitHub Actions 测试自动化部署</h2>
-  <div>
+    <h3>测试本地修改完代码推送到远程仓库 同时action的workflow工作流监听main分支更改重新打包为dist文件部署到gh-pages分支上</h3>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
+  </div> -->
   <HelloWorld msg="Vite + Vue" />
 </template>
 
