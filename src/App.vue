025072c464fdaee69c16e82fae02455e9b0fd070
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-05 21:33:10 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <h1>CI CD 构建成功</h1>
     <h2>GitHub Actions 测试自动化部署</h2>
     <h3>测试本地修改完代码推送到远程仓库 同时action的workflow工作流监听main分支更改重新打包为dist文件部署到gh-pages分支上</h3>
+    <h4>使用GitHub pages部署适合静态页面 动态会存在https协议的页面下发起http请求会被block等问题</h4>
   <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
